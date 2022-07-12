0f063d7b4f4f169390e43e17f868c686ea9039ff
yasin çınar <yasin001905@gmail.com>
2022-07-12 17:49:46 +0300

    layers Created

---
+++ Infrastructure/ECommerceAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using ECommerceAPI.Application.Abstractions;
+using ECommerceAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new() 
+        {
+            new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product 1",
+                Description = "Product 1 description",
+                Price = 10,
+                Quantity = 10
+            },
+            new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product 2",
+                Description = "Product 2 description",
+                Price = 20,
+                Quantity = 20
+            },
+            new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Product 3",
+                Description = "Product 3 description",
+                Price = 30,
+                Quantity = 30
+            }
+        };
+    }
+}
